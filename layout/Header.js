279097d410c1f171a6cc74b60eea86526f3fb7a8
nguyenkhanh28122000 <khanhnguyen28122000@gmail.com>
2023-01-17 13:48:52 +0700

    'base 2'

--- layout/Header.js
+++ layout/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import Link from 'next/link';
+import Image from 'next/image';
+import style from '@/styles/header.module.scss';
+
+import images from '@/styles/images';
+
+const cx = classNames.bind(style);
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    console.log(images);
 
-export default Header
+    return (
+        <div className={cx('wrapper')}>
+            <Link href={'/'}>
+                <Image src={images.LOGO} alt={'errorLogo'} width={100} height={40} priority className={cx('logo')} />
+            </Link>
+        </div>
+    );
+};
+
+export default Header;
---
+++ styles/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    LOGO: require('./logos/logo-b-1.png'),
+    LOGO2: require('./logos/logo-w-2.png'),
+};
+
+export default images;
