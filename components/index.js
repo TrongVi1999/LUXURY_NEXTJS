3e7be5f1d9d367063e235e324b2d3465752da00f
nguyenkhanh28122000 <khanhnguyen28122000@gmail.com>
2023-01-17 18:44:40 +0700

    'banner, header, info'

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { default as Section } from './Section';
 export { default as Button } from './Button';
 export { default as Input } from './Input';
+// export { default as SwiperSlideComp } from './SwiperSlideComp';
+// export { default as SwiperSlideComp } from './SwiperSlideComp/index';
--- styles/images/index.js
+++ styles/images/index.js
@@ -3,4 +3,20 @@ const images = {
     LOGO2: require('./logos/logo-w-2.png'),
 };
 
-export default images;
+const banners = {
+    banner1: require('./banner/banner-vinpearl.png'),
+    banner2: require('./banner/doi-cat-mui-ne-phan-thiet-dep-02.jpg'),
+    banner3: require('./banner/kinh-nghiem-du-lich-da-nang-1.jpg'),
+};
+
+const iconInfos = {
+    icon1: require('./icons/iconInfo1.jpg'),
+    icon2: require('./icons/iconInfo2.jpg'),
+    icon3: require('./icons/iconInfo3.jpg'),
+    icon4: require('./icons/iconInfo4.jpg'),
+    icon5: require('./icons/iconInfo5.jpg'),
+    icon6: require('./icons/iconInfo6.jpg'),
+    icon7: require('./icons/iconInfo7.jpg'),
+};
+
+export { images, banners, iconInfos };
