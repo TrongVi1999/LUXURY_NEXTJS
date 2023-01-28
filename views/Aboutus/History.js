f6976652ddc5f136c198e2ff26c3e9fab586c304
Phu121091 <nguyentrongphuu@gmail.com>
2023-01-28 09:52:33 +0700

    m

---
+++ views/Aboutus/History.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import style from './style.module.scss';
+
+const cx = classNames.bind(style);
+
+const History = () => {
+    return (
+        <div className={cx('container-story')}>
+
+        </div>
+    )
+}
+
+export default History
