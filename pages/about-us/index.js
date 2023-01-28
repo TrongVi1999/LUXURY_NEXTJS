815f3e64ea085b62e1dd06acb5327b764a5e0947
nguyenkhanh28122000 <khanhnguyen28122000@gmail.com>
2023-01-28 11:12:27 +0700

    'about out team'

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import classNames from 'classnames/bind';
+import styles from '@/styles/aboutUs.module.scss';
+
+import { banners } from '@/public/images';
+import { ItemInfoHome, BannerSlide } from '@/views';
+import { Section } from '@/components';
+import { OutTeamAboutUs } from '@/views';
+const cx = classNames.bind(styles)
+
 const index = () => {
     return (
-        <div>index</div>
+        <>
+            <div className={cx('wrapperBanner')}>
+                <BannerSlide
+                    className={cx('bodyBanner')}
+                    imgBanner={[banners.banner4, banners.banner6, banners.banner7, banners.banner8]}
+                />
+                <ItemInfoHome className={cx('boxInfo')} />
+            </div>
+            <Section backgroundImg={banners.banner1.default.src} notPadding>
+                <div className={cx('bannerAboutUs')}></div>
+            </Section>
+            <Section notPadding>
+                <OutTeamAboutUs />
+            </Section>
+        </>
     )
 }
 
--- views/index.js
+++ views/index.js
@@ -2,3 +2,4 @@ export { default as Banner } from './BannerSlide/Banner/index';
 export { default as ItemInfoHome } from './ItemInfoHome/index';
 export { default as BannerSlide } from './BannerSlide/index';
 export { default as CategoryFilter } from './CategoryFilter/index';
+export { default as OutTeamAboutUs } from './Aboutus/OutTeam/index';
