29ee02b66ca7f7ffff45abdc766c76d6db5c35fc
kenvir <viet02092001@gmail.com>
2023-01-30 17:56:01 +0700

    infor-booking

--- pages/information-booking/index.js
+++ pages/information-booking/index.js
@@ -27,167 +27,185 @@ function index() {
             <BannerIMG img={IMG} title='U N S E E N  V I E T N A M' bg='bg' className={cx('book-title')} />
 
             <div className={cx("booking-infor")}>
-            <div className={cx("book-crumb")}>Home | BOOK TOUR</div>
-            <div className={cx("book-content")}>
-                <div className={cx("content-header")}>
-                    <p className={cx("tour-name")}>
-                        Tour Name:&nbsp;
-                        <span className={cx("tour-name-content")}>
-                            UNSEEN VIETNAM
-                        </span>
-                    </p>
-                    <p className={cx("tour-duration")}>
-                        Tour duration:&nbsp;
-                        <span className={cx("tour-duration-content")}>
-                            3 days
-                        </span>
-                    </p>
-                    <p className={cx("tour-country")}>
-                        Country:&nbsp;
-                        <span className={cx("tour-country-content")}>
-                            VIETNAM
-                        </span>
-                    </p>
-                </div>
-                <hr className={cx("line")}></hr>
-                <div className={cx("content-mid")}>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Your proposed arrival date:
-                        </label>
-                        <Input type="date" classNameInput={cx("book-date")} />
+                <div className={cx("book-crumb")}>Home | BOOK TOUR</div>
+                <div className={cx("book-content")}>
+                    <div className={cx("content-header")}>
+                        <p className={cx("tour-name")}>
+                            Tour Name:&nbsp;
+                            <span className={cx("tour-name-content")}>
+                                UNSEEN VIETNAM
+                            </span>
+                        </p>
+                        <p className={cx("tour-duration")}>
+                            Tour duration:&nbsp;
+                            <span className={cx("tour-duration-content")}>
+                                3 days
+                            </span>
+                        </p>
+                        <p className={cx("tour-country")}>
+                            Country:&nbsp;
+                            <span className={cx("tour-country-content")}>
+                                VIETNAM
+                            </span>
+                        </p>
                     </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            How many people in your group?
-                        </label>
-                        <div className={cx("age-option")}>
-                            <Input
-                                type="text"
-                                placeholder="Adult(s) (>=12 years old)"
-                                classNameInput={cx("book-age")}
-                            />
-                            <Input
-                                type="text"
-                                placeholder="Child(ren) (7-11 years old)"
-                                classNameInput={cx("book-age")}
-                            />
-                            <Input
-                                type="text"
-                                placeholder="Infant(s) (0-2 years old)"
-                                classNameInput={cx("book-age")}
-                            />
-                            <Input
-                                type="text"
-                                placeholder="Child(ren) (2-6 years old)"
-                                classNameInput={cx("book-age")}
-                            />
+                    <hr className={cx("line")}></hr>
+                    <div className={cx("content-mid")}>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Your proposed arrival date:
+                            </label>
+                            <Input type="date" classNameInput={cx("book-date")} />
                         </div>
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Hotel categories you desire to stay?
-                        </label>
-                        <Input
-                            // isSelect={isSelect}
-                            classNameInput={cx("book-hotel")}
-                        />
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            How should we call you? (*)
-                        </label>
-                        <div className={cx("cus-infor")}>
-                            <Input
-                                type="text"
-                                placeholder="Enter Your Name"
-                                classNameInput={cx("cus-name")}
-                            />
-                            <div className={cx("sex")}>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                How many people in your group?
+                            </label>
+                            <div className={cx("age-option")}>
                                 <Input
-                                    name="gender"
-                                    type="checkbox"
-                                    value="male"
-                                    classNameInput={cx("form-control")}
+                                    type="text"
+                                    placeholder="Adult(s) (>=12 years old)"
+                                    classNameInput={cx("book-age")}
                                 />
-                                <label className={cx("sex-m")} for="">
-                                    MALE
-                                </label>
                                 <Input
-                                    name="gender"
-                                    type="checkbox"
-                                    value="female"
-                                    classNameInput={cx("form-control")}
+                                    type="text"
+                                    placeholder="Child(ren) (7-11 years old)"
+                                    classNameInput={cx("book-age")}
                                 />
-                                <label className={cx("sex-m")} for="">
-                                    FEMALE
-                                </label>
+                                <Input
+                                    type="text"
+                                    placeholder="Infant(s) (0-2 years old)"
+                                    classNameInput={cx("book-age")}
+                                />
+                                <Input
+                                    type="text"
+                                    placeholder="Child(ren) (2-6 years old)"
+                                    classNameInput={cx("book-age")}
+                                />
                             </div>
                         </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Hotel categories you desire to stay?
+                            </label>
+                            <Input
+                                // isSelect={isSelect}
+                                classNameInput={cx("book-hotel")}
+                            />
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                How should we call you? (*)
+                            </label>
+                            <div className={cx("cus-infor")}>
+                                <Input
+                                    type="text"
+                                    placeholder="Enter Your Name"
+                                    classNameInput={cx("cus-name")}
+                                />
+                                <div className={cx("sex")}>
+                                    <Input
+                                        name="gender"
+                                        type="checkbox"
+                                        value="male"
+                                        classNameInput={cx("form-control")}
+                                    />
+                                    <label className={cx("sex-m")} for="">
+                                        MALE
+                                    </label>
+                                    <Input
+                                        name="gender"
+                                        type="checkbox"
+                                        value="female"
+                                        classNameInput={cx("form-control")}
+                                    />
+                                    <label className={cx("sex-m")} for="">
+                                        FEMALE
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Your nationality:
+                            </label>
+                            <Input
+                                type="select"
+                                classNameInput={cx("book-national")}
+                            />
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Email address (*):
+                            </label>
+                            <Input
+                                type="text"
+                                placeholder="Enter Your Email"
+                                classNameInput={cx("cus-mail")}
+                                {...register('Email', {
+                                    required: true,
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    },
+                                })}
+                            />
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Confirm Email Contact (*):
+                            </label>
+                            <Input
+                                type="text"
+                                placeholder="Confirm Email"
+                                classNameInput={cx("cus-mail")}
+                                {...register('Email', {
+                                    required: true,
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    },
+                                })}
+                            />
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                Do you expect a phone call?
+                            </label>
+                            <Input
+                                type="text"
+                                placeholder="Enter Your Phone"
+                                classNameInput={cx("cus-phone")}
+                                {...register('Phone', {
+                                    required: true,
+                                    minLength: 9,
+                                    maxLength: 15,
+                                    valueAsNumber: false,
+                                })}
+                            />
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                We welcome your special requests here
+                            </label>
+                            <textarea
+                                placeholder="Type here for special activities, alergy, wheel chair, vegetari"
+                                className={cx("book-note")}
+                            ></textarea>
+                        </div>
+                        <div className={cx("item-form")}>
+                            <label className={cx("label-booking")}>
+                                How did you hear about our services?
+                            </label>
+                            <Input
+                                type="select"
+                                classNameInput={cx("our-services")}
+                            />
+                        </div>
                     </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Your nationality:
-                        </label>
-                        <Input
-                            type="select"
-                            classNameInput={cx("book-national")}
-                        />
+                    <div className={cx("content-bot")}>
+                        <button className={cx("btn")}>SUMMIT</button>
                     </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Email address (*):
-                        </label>
-                        <Input
-                            type="text"
-                            placeholder="Enter Your Email"
-                            classNameInput={cx("cus-mail")}
-                        />
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Confirm Email Contact (*):
-                        </label>
-                        <Input
-                            type="text"
-                            placeholder="Confirm Email"
-                            classNameInput={cx("cus-mail")}
-                        />
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            Do you expect a phone call?
-                        </label>
-                        <Input
-                            type="text"
-                            placeholder="Enter Your Phone"
-                            classNameInput={cx("cus-phone")}
-                        />
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            We welcome your special requests here
-                        </label>
-                        <textarea
-                            placeholder="Type here for special activities, alergy, wheel chair, vegetari"
-                            className={cx("book-note")}
-                        ></textarea>
-                    </div>
-                    <div className={cx("item-form")}>
-                        <label className={cx("label-booking")}>
-                            How did you hear about our services?
-                        </label>
-                        <Input
-                            type="select"
-                            classNameInput={cx("our-services")}
-                        />
-                    </div>
-                </div>
-                <div className={cx("content-bot")}>
-                    <button className={cx("btn")}>SUMMIT</button>
                 </div>
             </div>
-        </div>
             <div className={cx('tour-same')}>
                 <span className={cx('tour-same-title')}>TOUR TƯƠNG TỰ</span>
                 <Tourrecomment data={data} />
