0c23b9950bf55d4248121e262feda697ea27c660
kenvir <viet02092001@gmail.com>
2023-01-31 09:53:13 +0700

    fix infor-booking

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'vnxpedia.3i.com.vn',
-        port: '',
-        pathname: '/uploads/images/**',
-      },
-    ],
-  },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'vnxpedia.3i.com.vn',
+                port: '',
+                pathname: '/uploads/images/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
