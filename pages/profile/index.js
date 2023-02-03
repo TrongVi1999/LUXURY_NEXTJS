9af8562a0cc6fc2d5aba1a989d306141481d0d92
truonghvph16694 <96322678+truonghvph16694@users.noreply.github.com>
2023-02-03 16:00:02 +0700

    new hoteldetail

---
+++ pages/profile/index.js
@@ -0,0 +1,12 @@
+import UserAvatar from '@/components/UserAvatar'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            < UserAvatar />
+        </div>
+    )
+}
+
+export default Index
