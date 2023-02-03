04e4ffc75130e6e360b09e50f4b79ee811fe8051
kenvir <viet02092001@gmail.com>
2023-02-03 13:39:05 +0700

    1:40h 3/2

--- views/LuxuryTrans/BoxCar.js
+++ views/LuxuryTrans/BoxCar.js
@@ -3,10 +3,10 @@ import style from './styleTransFer.module.scss'
 import Image from "next/image";
 const cx = classNames.bind(style)
 
-function BoxCar({ img, name, className, key }) {
+function BoxCar({ img, name, className, key, click }) {
     return (<div key={key} className={cx('wrapperBoxCar', { [className]: className })}>
         <div className={cx('boxImg')}>
-            <Image className={cx('img')} src={img.default.src} alt="carImgErr" layout="fill" />
+            <Image className={cx('img')} src={img.default.src} alt="carImgErr" layout="fill"/>
         </div>
         <h3 className={cx('nameCar')}>{name}</h3>
     </div>);
