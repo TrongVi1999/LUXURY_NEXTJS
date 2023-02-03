03be1c46c508fbe48042274ce30e5c4cd253597e
nguyenkhanh28122000 <khanhnguyen28122000@gmail.com>
2023-02-03 08:17:38 +0700

    'api blog detail'

---
+++ views/Blogdetail/index.js
@@ -0,0 +1,5 @@
+export { default as Author } from './Author'
+export { default as Blogrecomment } from './Blogrecomment'
+
+export { default as Comments } from './Comments'
+export { default as WriteComment } from './WriteComment'
