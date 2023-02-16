790da9a1d3824e9d68d7e10edc36e9fa173e8d9d
truonghvph16694 <96322678+truonghvph16694@users.noreply.github.com>
2023-02-16 11:35:42 +0700

    new

---
+++ views/HotelDetail/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import style from '@/styles/Hoteldetail.module.scss';
+import classNames from 'classnames/bind';
+import Link from 'next/link';
+import Image from 'next/image';
+import { AiFillStar } from 'react-icons/ai';
+import Loading from '@/components/Loading';
+import { ListHotel } from '@/pages/api/QuerryAPI';
+
+const cx = classNames.bind(style);
+
+const HotelDetail = () => {
+    const getHotelDetail = ListHotel()
+    if (getHotelDetail.isLoading) {
+        return <Loading />;
+    }
+
+    if (getHotelDetail.error) {
+        return <p>Error: {error.message}</p>;
+    }
+    console.log(getHotelDetail.data);
+    return (
+
+
+
+        <div className={cx('Siminal-Hotel')}>
+            {getHotelDetail.data && getHotelDetail.data.Object.slice(7, 10).map((d, i) =>
+                <Link href={`/hotel-detail/5258=`} className={cx('card')} key={i}>
+                    <div className={cx('card-img')} >
+                        <Image src={`https://vnxpedia.3i.com.vn${d.gallery}`} alt="vnxpedia-tour-img" className={cx('img')} width={200} height={200} />
+                    </div>
+                    <div className={cx('infor')}>
+                        <h6 className={cx('title')}>{d.title.toUpperCase()}</h6>
+                        <p className={cx('rate')}>
+                            <span className={cx('rating')}>
+                                <AiFillStar /> 4.8
+                            </span>
+                            <span className={cx('ratecount')}>
+                                &#40; 28.091 Đánh giá &#41; |600 Đã được đặt
+                            </span>
+                        </p>
+                        <p className={cx('price')}>
+                            {/* <span className={cx('price1')}>$ {data.price}</span> */}
+
+                        </p>
+                    </div>
+                </Link>
+            )
+            }
+        </div>
+    )
+}
+
+export default HotelDetail
