463db879df9e96fdd8eaaac8c1cbf63a8a76e858
Phu121091 <nguyentrongphuu@gmail.com>
2023-02-26 15:28:41 +0700

    .

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
     },
     images: {
         unoptimized: true,
-        // remotePatterns: [
-        //     {
-        //         protocol: 'https',
-        //         hostname: 'vnxpedia.3i.com.vn',
-        //         port: '',
-        //         pathname: '/uploads/images/**',
-        //     },
-        // ],
         domains: [
             'vnxpedia.3i.com.vn',
         ],
@@ -21,3 +13,61 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = express()
+
+    // Handle dynamic routes
+    server.get('/way-to-travel/:id', (req, res) => {
+        const actualPage = '/way-to-travel/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/destination/:id', (req, res) => {
+        const actualPage = '/destination/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/tour-detail/:id', (req, res) => {
+        const actualPage = '/tour-detail/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/hotel-detail/:id', (req, res) => {
+        const actualPage = '/hotel-detail/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/transfer-detail/:id', (req, res) => {
+        const actualPage = '/transfer-detail/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/blog-detail/:id', (req, res) => {
+        const actualPage = '/blog-detail/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+    server.get('/payment/:id', (req, res) => {
+        const actualPage = '/payment/[id]'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
+
+    // Handle all other routes
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(3000, err => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+})
