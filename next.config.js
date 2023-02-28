70516723c70d6599461f3ed7831af7e221adc56e
Phu121091 <nguyentrongphuu@gmail.com>
2023-02-28 13:43:24 +0700

    m

--- next.config.js
+++ next.config.js
@@ -10,17 +10,17 @@ const nextConfig = {
             'vnxpedia.3i.com.vn',
         ],
     },
-    async rewrites() {
-        return [
-            { source: '/destination', destination: '/destination.html' },
-            { source: '/way-to-travel', destination: '/way-to-travel.html' },
-            { source: '/tour-detail', destination: '/tour-detail.html' },
-            { source: '/hotel-detail', destination: '/hotel-detail.html' },
-            { source: '/transfer-detail', destination: '/transfer-detail.html' },
-            { source: '/payment', destination: '/payment.html' },
-            { source: '/blog-detail', destination: '/blog-detail.html' },
-        ]
-    },
+    // async rewrites() {
+    //     return [
+    //         { source: '/destination', destination: '/destination.html' },
+    //         { source: '/way-to-travel', destination: '/way-to-travel.html' },
+    //         { source: '/tour-detail', destination: '/tour-detail.html' },
+    //         { source: '/hotel-detail', destination: '/hotel-detail.html' },
+    //         { source: '/transfer-detail', destination: '/transfer-detail.html' },
+    //         { source: '/payment', destination: '/payment.html' },
+    //         { source: '/blog-detail', destination: '/blog-detail.html' },
+    //     ]
+    // },
 }
 
 module.exports = nextConfig;
