06cb9aeb4913546e792cd10dc009f7ed7fb06ac4
Phu121091 <nguyentrongphuu@gmail.com>
2023-03-05 14:49:01 +0700

    m

--- components/Head/head.js
+++ components/Head/head.js
@@ -8,7 +8,6 @@ const Headpage = ({ title, descrip }) => {
             <meta name="description" content={descrip ? descrip : `Vietnam Luxury Tours I Wellness Vietnam I Adventure Travel & Incentive Tailor-made Trips`} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" type="image/png" sizes="20x20" href={Logo.src} />
-            <link rel="canonical" href="https://www.luxuryvietravel.com/" />
         </Head>
     )
 }
