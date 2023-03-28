1ee240ba7faae68b3247a248b9a4f5deee5a3486
Phu121091 <nguyentrongphuu@gmail.com>
2023-03-28 19:12:22 +0700

    m

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WX3T15YC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-N9WX3T15YC');
+              `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
