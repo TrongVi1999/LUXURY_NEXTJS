a5f596bc90fb2fedfaa15ce1b8e79b41d01c2657
Trong Vi <vutrongvi96@gmail.com>
2023-05-13 19:04:28 +0700

    update1

--- views/Home/Start.js
+++ views/Home/Start.js
@@ -12,14 +12,13 @@ const Start = () => {
             <div className={cx('text')}>
                 <h4>We know luxury holidays</h4>
                 <p>Unparalled, tailor-made luxury trips with expert service .For Us ,Every Thing ,Matter</p>
-                <p>Now let's start your first step!</p>
+                <p>Now let's start your first step.Start a memorable experience!</p>
                 <div className={cx('grid-btn')}>
                     <button className={cx('button-style')}>Most Popular</button>
                     <button className={cx('button-style')}>By Destination</button>
                     <button className={cx('button-style')}>By Type</button>
                     <button className={cx('button-style')}>View All</button>
                 </div>
-                <p>Start a memorable experience!</p>
             </div>
         </div>
     )
